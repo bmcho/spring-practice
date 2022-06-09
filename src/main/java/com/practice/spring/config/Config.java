c545a3deca20c4868b41d0ad9aed2d51575ce022
bmcho <bmcho@naver.com>
2022-06-09 16:52:52 +0900

    Close #1 spring practice end

--- src/main/java/com/practice/spring/config/Config.java
+++ src/main/java/com/practice/spring/config/Config.java
@@ -2,8 +2,10 @@ package com.practice.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.practice.spring")
+@EnableWebMvc
 public class Config {
 }
